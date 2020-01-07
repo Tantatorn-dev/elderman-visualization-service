432cfcd26b4a5e09b4db5d6e1726423c7e306ee3
Tantatorn <timefear007@gmail.com>
2020-01-08 00:39:09 +0700

    show map of thailand

--- src/components/DustMap/DustMap.js
+++ src/components/DustMap/DustMap.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const MyPopupMarker = ({ content, position }) => (
+    <Marker position={position}>
+        <Popup>{content}</Popup>
+    </Marker>
+)
+
+const MyMarkersList = ({ markers }) => {
+    const items = markers.map(({ key, ...props }) => (
+        <MyPopupMarker key={key} {...props} />
+    ))
+    return <Fragment>{items}</Fragment>
+}
 
 export default function DustMap() {
 
-    const position = [51.505, -0.09]
-    const zoom = 13
+    const markers = [
+        { key: 'marker1', position: [51.5, -0.1], content: 'My first popup' },
+        { key: 'marker2', position: [51.51, -0.1], content: 'My second popup' },
+        { key: 'marker3', position: [51.49, -0.05], content: 'My third popup' },
+    ]
+    const position = [15.8700, 100.9925]
+    const zoom = 6
 
     return (
-        <Map center={position} zoom={zoom}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-            </Marker>
-        </Map>
+            <Map center={position} zoom={zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <MyMarkersList markers={markers} />
+            </Map>
     )
 }
