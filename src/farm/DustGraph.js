a34c251d20768c3feeb276846d342d2bea027018
Kittisak <dnjooiopa@gmail.com>
2020-01-07 22:41:03 +0700

    add pm2.5 to json day

--- src/farm/DustGraph.js
+++ src/farm/DustGraph.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, LinearProgress } from "@material-ui/core";
+import Plot from "react-plotly.js";
 
 import extractData from "./data";
 
@@ -22,10 +23,11 @@ const DustGraph = () => {
         fetch("https://tgr2020-quiz2.firebaseio.com/quiz/sensor/team32.json")
             .then(res => res.json())
             .then(res => {
-                extractData(res, function(xData, yData) {
+                extractData(res, function(xData, yData, avgPerDay) {
                     let temp = { x: xData, y: yData };
                     setData(temp);
                     setIsLoading(false);
+                    console.log(avgPerDay);
                 });
             })
             .catch(error => {
@@ -40,7 +42,31 @@ const DustGraph = () => {
     return (
         <Container>
             {data ? (
-                <div>Success</div>
+                <Plot
+                    data={[
+                        {
+                            x: data.x,
+                            y: data.y,
+                            type: "scatter",
+                            mode: "lines+markers",
+                            marker: { color: "red" }
+                        }
+                    ]}
+                    layout={{
+                        width: 1000,
+                        height: 500,
+                        title: "PM2.5 data analyzing",
+                        xaxis: {
+                            title: "Date",
+                            showgrid: false,
+                            tickangle:90
+                        },  
+                        yaxis: {
+                            title: "PM 2.5 concentration",
+                            showline: true
+                        }
+                    }}
+                />
             ) : (
                 <div className={classes.root}>
                     <LinearProgress />
--- src/farm/data.js
+++ src/farm/data.js
@@ -1,17 +1,25 @@
 export default function extractData(data, callback) {
-    let sensorData = []
-    let timeData = []
+    let sensorData = [];
+    let timeData = [];
+    let averagePerDay = {};
     Object.keys(data).map((key, index) => {
-        if (Object.keys(data[key])[0] === 'DevEUI_uplink') {
-            let temp = data[key]['DevEUI_uplink']
-            if (temp['DevAddr'] === '14EF1432') {
-                let sensorVal = Number.parseInt(temp['payload_hex'].substring(2, 4), 16)
+        if (Object.keys(data[key])[0] === "DevEUI_uplink") {
+            let temp = data[key]["DevEUI_uplink"];
+            if (temp["DevAddr"] === "14EF1432") {
+                let sensorVal = Number.parseInt(temp["payload_hex"].substring(2, 4), 16);
                 if (sensorVal != 191) {
-                    sensorData.push(sensorVal)
-                    timeData.push(new Date(temp['Time']))
+                    sensorData.push(sensorVal);
+                    const date = new Date(temp["Time"]);
+                    timeData.push(date);
+                    const dateStr = date.toDateString();
+                    if (averagePerDay.hasOwnProperty(dateStr)) {
+                        averagePerDay[dateStr].push(sensorVal);
+                    } else {
+                        averagePerDay[dateStr] = [sensorVal];
+                    }
                 }
             }
         }
-    })
-    callback(timeData, sensorData)
+    });
+    callback(timeData, sensorData, averagePerDay);
 }
