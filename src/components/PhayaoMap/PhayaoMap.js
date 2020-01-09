43bd8315db6866fa2d631be707813733b67dc65d
Tantatorn <timefear007@gmail.com>
2020-01-09 20:35:25 +0700

    add group number to a marker

--- src/components/PhayaoMap/PhayaoMap.js
+++ src/components/PhayaoMap/PhayaoMap.js
@@ -9,8 +9,23 @@ export default function PhayaoMap() {
 
     const zoom = 10
 
+    const checkGroup = (deviceID) => {
+        if (deviceID == 47) {
+            return 'team32'
+        }
+        else if (deviceID == 46) {
+            return 'team1'
+        }
+        else if (deviceID == 45) {
+            return 'team2'
+        }
+        else if (deviceID == 44) {
+            return 'team29'
+        }
+    }
+
     const extractData = async (data) => {
-        posState.push([data['lat'], data['lon'], data['value']])
+        posState.push([data['lat'], data['lon'], data['value'], checkGroup(data['device_id'])])
 
         setPosState([...posState])
     }
@@ -34,33 +49,33 @@ export default function PhayaoMap() {
             fetch('http://202.139.192.221/api/pm25_data/46')
                 .then(res => res.json())
                 .then(data => extractData(data))
-            
+
         }, 5000)
 
     }, []
     )
 
     return (
-        <div>
-            <Map center={position} zoom={zoom}>
-                <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {
-                    posState.map((item, index) => {
-                        if (!(item[0] === undefined && item[1] === undefined)) {
-                            return (
-                                <Marker key={index} position={item}>
-                                    <Popup>
-                                        <span key={index}>{item[2]} µg/m3</span>
+            <div style={{ paddingTop: 40 }}>
+                <Map center={position} zoom={zoom}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {
+                        posState.map((item, index) => {
+                            if (!(item[0] === undefined && item[1] === undefined)) {
+                                return (
+                                    <Marker key={index} position={[item[0], item[1]]} >
+                                        <Popup>
+                                            <span key={index}>{item[2]} µg/m3 {item[3]}</span>
                                         </Popup>
-                                </Marker>
-                            )
-                        }
-                    })
-                }
-            </Map>
-        </div>
+                                    </Marker>
+                                )
+                            }
+                        })
+                    }
+                </Map>
+            </div>
     )
 }
