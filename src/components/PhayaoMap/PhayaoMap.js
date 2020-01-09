c5011206fcf949163d92d194902133bdc0dbb478
Tantatorn <timefear007@gmail.com>
2020-01-09 23:50:02 +0700

    add current location marker

--- src/components/PhayaoMap/PhayaoMap.js
+++ src/components/PhayaoMap/PhayaoMap.js
@@ -1,9 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet';
+import L from "leaflet";
+
+const CurrentLocationCircle = ({ s }) => {
+    if (s == 1)
+        return (
+            <CircleMarker
+                center={[19.021294, 99.897926]}
+                fillColor="green"
+                radius={20} />)
+    if (s == 2)
+        return (
+            <CircleMarker
+                center={[19.176477, 99.88926]}
+                fillColor="green"
+                radius={20} />)
+    if (s == 3)
+        return (
+            <CircleMarker
+                center={[19.177149, 99.812571]}
+                fillColor="green"
+                radius={20} />)
+    if (s == 4)
+        return (
+            <CircleMarker
+                center={[19.166391, 99.901908]}
+                fillColor="green"
+                radius={20} />)
+}
 
 export default function PhayaoMap() {
 
     const [posState, setPosState] = useState([])
+
+    const [currentPos, setCurrentPos] = useState(1)
 
     const position = [19.1667, 99.8667]
 
@@ -30,7 +60,31 @@ export default function PhayaoMap() {
         setPosState([...posState])
     }
 
+    const handleEvent = async (data) => {
+        if (data['event_code'] == 8) {
+            console.log('death')
+        }
+        else if (data['event_code'] == 128) {
+            console.log('fall')
+        }
+        else if (data['event_code'] == 255) {
+            console.log('fall_slide')
+        }
+        else 
+        {
+            console.log('normal')
+        }
+    }
+
     useEffect(() => {
+
+        setInterval(() => {
+            fetch('http://202.139.192.221/api/predict')
+                .then(res => res.json())
+                .then(data => {
+                    setCurrentPos(data)
+                })
+        }, 1000)
 
         setInterval(() => {
 
@@ -51,32 +105,38 @@ export default function PhayaoMap() {
                 .then(data => extractData(data))
 
         }, 5000)
-
+        
+                setInterval(() => {
+                    fetch('http://202.139.192.221/api/track_data/test')
+                        .then(res => res.json())
+                        .then(data => handleEvent(data))
+                }, 1000)
     }, []
     )
 
     return (
-            <div style={{ paddingTop: 40 }}>
-                <Map center={position} zoom={zoom}>
-                    <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    {
-                        posState.map((item, index) => {
-                            if (!(item[0] === undefined && item[1] === undefined)) {
-                                return (
-                                    <Marker key={index} position={[item[0], item[1]]} >
-                                        <Popup>
-                                            <span key={index}><span style={{fontWeight:"bold"}}>team:</span> {item[3]}</span><br/>
-                                            <span key={index}><span style={{fontWeight:"bold"}}>PM2.5 value</span> {item[2]} µg/cubic meter</span><br/>
-                                        </Popup>
-                                    </Marker>
-                                )
-                            }
-                        })
-                    }
-                </Map>
-            </div>
+        <div style={{ paddingTop: 40 }}>
+            <Map center={position} zoom={zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    posState.map((item, index) => {
+                        if (!(item[0] === undefined && item[1] === undefined)) {
+                            return (
+                                <Marker key={index} position={[item[0], item[1]]} >
+                                    <Popup key={index}>
+                                        <span key={index}><span style={{ fontWeight: "bold" }}>team:</span> {item[3]}</span><br />
+                                        <span key={index}><span style={{ fontWeight: "bold" }}>PM2.5 value</span> {item[2]} µg/cubic meter</span><br />
+                                    </Popup>
+                                </Marker>
+                            )
+                        }
+                    })
+                }
+                <CurrentLocationCircle s={currentPos} />
+            </Map>
+        </div>
     )
 }
